986747464e296ebf67a9ee10f199821aca196ce6
Deyan Iliev <98272349+DeyanIliev@users.noreply.github.com>
2022-05-26 20:54:26 +0300

    Inserting starter things
    Adding started things like:
    first code
    folders
    ideas

---
+++ InsertNameOfTheBook/Code/ProjectKAPPA/src/skills.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class skills {
+    // Diplomacy
+    // Leadership
+    // Riding
+    // Archery
+    // Melee-weapons
+    // Knowledge
+    // Strategy
+    public static String Skills(skills){
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String a = "Diplomacy";
+        String b = "Leadership";
+        String c = "Riding";
+        String d = "Archery";
+        String e = "Melee-weapons";
+        String f = "Knowledge";
+        String g = "Strategy";
+
+        switch (input){
+            case "a":
+                break;
+            case "b":
+                break;
+            case "c":
+                break;
+            case "d":
+                break;
+            case "e":
+                break;
+            case "f":
+                break;
+            case "g":
+                break;
+        }
+
+        String skills = input;
+        return skills;
+    }
+}
